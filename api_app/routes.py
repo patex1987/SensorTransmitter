c9c4d9ab8b8513592692ea0874822fbd2e910b00
patex1987 <patex1987@gmail.com>
2018-01-27 08:37:56 +0100

    Better request error handling

--- api_app/routes.py
+++ api_app/routes.py
@@ -1,26 +1,50 @@
 from api_app import app
 from api_app import sensor
 from flask import request
-from flask import abort
-from flask import make_response
 from flask import jsonify
 
+
+class InvalidRequest(Exception):
+    '''Exception class for handling invalid requests
+    '''
+
+    def __init__(self, message, status_code=400):
+        '''InvalidRequest's __init__ method
+
+        Args:
+            message(str): error message
+            status_code (int): HTTP status code, default value:400 (bad
+                request)
+        '''
+        Exception.__init__(self)
+        self.message = message
+        self.status_code = status_code
+
+    def to_dict(self):
+        '''Converts the object into a dictionary, useful for JSON
+        serialization
+        '''
+        request_info = {}
+        request_info['error'] = self.message
+        return request_info
+
+
 @app.route('/sensor', methods=['GET'])
-def get_timezones():
-    '''handles timezone requests
+def get_sensor_values():
+    '''handles sensor requests
     '''
     arguments = request.args
-    if len(arguments) != 0:
-        abort(400)
-    actual_temp = sensor.actual_temperature
+    if arguments:
+        raise InvalidRequest(message='No paramaters accepted')
+    actual_temp = sensor.actual_value
     result_val = {'temperature': actual_temp}
     return jsonify(result_val)
 
 
-@app.errorhandler(400)
-def not_found(error):
-    '''error 400 handling
+@app.errorhandler(InvalidRequest)
+def handle_invalid_usage(error):
+    '''Handling invalid requests
     '''
-    error_msg = 'Error, wrong parameters'
-    error_output = jsonify({'error': error_msg})
-    return make_response(error_output, 400)
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
