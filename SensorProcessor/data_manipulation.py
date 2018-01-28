b5d2038e4fc1cf6ab3116c6e2c71cb14368a962f
patex1987 <patex1987@gmail.com>
2018-01-28 19:09:45 +0100

    XY ringbuffer added

--- SensorProcessor/data_manipulation.py
+++ SensorProcessor/data_manipulation.py
@@ -4,8 +4,10 @@ import requests
 from requests.exceptions import ConnectionError
 import numpy as np
 
+
 class NoDataError(Exception):
     pass
+
 
 class GetSensorsThread(QThread):
     def __init__(self, start_thread=False, update_interval_secs=1):
@@ -14,7 +16,7 @@ class GetSensorsThread(QThread):
         '''
         super().__init__()
         self.start_thread = start_thread
-        self.update_interval = update_interval_secs
+        self.update_interval_secs = update_interval_secs
 
     def __del__(self):
         self.wait()
@@ -43,6 +45,40 @@ class GetSensorsThread(QThread):
                 self.emit(SIGNAL('update_values(PyQt_PyObject)'),
                           actual_values)
                 print('IN THREAD')
-                self.sleep(self.update_interval_secs)
+                #self.sleep(self.update_interval_secs)
+                self.msleep(15)
             except NoDataError:
                 self.emit(SIGNAL('handle_error()'))
+
+
+class XYBuffer(object):
+    '''A class for holding X and Y about the sensor
+
+    Normally we would like to hold data for the last hour
+    This class can be used for plotting sensor data as a moving window
+    '''
+    def __init__(self, update_interval_secs, total_secs=3600,
+                 data_type=float):
+        '''XYBuffer constructor
+        '''
+        self.buffer_size = total_secs // update_interval_secs
+        self.x_buffer = RingBuffer(size_max=self.buffer_size,
+                                   default_value=0,
+                                   dtype=np.uint32)
+        self.y_buffer = RingBuffer(size_max=self.buffer_size,
+                                   default_value=0.0,
+                                   dtype=data_type)
+        self._x_counter = 0
+
+    def append_value(self, y_value):
+        '''Appends the y value to the y_buffer and increments the sample
+        number in the x_buffer
+        '''
+        self.y_buffer.append(y_value)
+        self._x_counter += 1
+        self.x_buffer.append(self._x_counter)
+
+    def get_actual_filled_values(self):
+        '''Gets the values from the buffer, which are already filled
+        '''
+        return self.x_buffer.partial, self.y_buffer.partial
